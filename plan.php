59e8467da86d9306ce0f49cf38c27bdea5f2b677
ChengEn <45934743+peter6098790@users.noreply.github.com>
2021-06-13 23:26:52 +0800

    Add files via upload

--- plan.php
+++ plan.php
@@ -10,27 +10,7 @@ $counter = -1;
 //         $counter = $counter + 1;
         
 // }
-foreach ($_POST as $value) {
-  $counter = $counter + 1;
-}
-#var_dump($_POST);
-$target = $_POST['target'];
-echo '目標:'.$target;
-echo '<br/>';
-#unset($_POST['target']);
-#print($counter.'<br>');
-$index_counter = $counter/2;
-//Get the POST data from insertView
-for($i = 1; $i <= $index_counter ; $i = $i+1) {
-    $index_week =  'a'.(string)$i;
-    $index_demand =  'b'.(string)$i;
-    echo '周次: '.$_POST[$index_week];
-    echo '需求: '.$_POST[$index_demand];
-    echo '<br/>';
-    //Check if Product has been in the database
-    //True -> Update Product info ; False-> Create New Product
-    //計算需求
-}
+
 
 class product
 {
@@ -49,7 +29,7 @@ class product
 
     $tmpArr = [];
     $result = readAllData('');
-    $counter = 0;
+    $counter = -1;
     //read data from DB
     while($rs =mysqli_fetch_assoc($result)){
         #echo '<br/>';
@@ -63,8 +43,12 @@ class product
         ${''.$name} -> name = $rs['pname'];
         ${''.$name} -> stock = $rs['stock'];
         ${''.$name} -> leadtime = $rs['leadtime'];
-        ${''.$name} -> materialStringArr = explode(",",$rs['material_name']);
-        ${''.$name} -> materialnumber = explode(",",$rs['material_qty']);
+        //確定有子零件才做字串分割,不然會存看不到的神奇數值進去
+        if ($rs['material_name'] != null){
+            ${''.$name} -> materialStringArr = explode(",",$rs['material_name']);
+            ${''.$name} -> materialnumber = explode(",",$rs['material_qty']);
+        }
+        #echo $rs['material_name'].'<br/>';
     }
 
     foreach($tmpArr as $value){
@@ -83,7 +67,7 @@ class product
         ${''.$value} -> materialList = $Arr;
     }
     function addlevel($product,$tmplevel){
-        if (isset($product -> materialList)){
+        if ($product -> materialList != null){ #isset($product -> materialList)
             if ($product -> level < $tmplevel){
                 $product -> level = $tmplevel;
             }
@@ -105,21 +89,23 @@ class product
         $leadtime = 0;
         $tmpleadtime = [];
         //半成品or成品
-        if (isset($product -> materialList)){
+        if (isset($product -> materialList)){ #$product -> materialList != null
             #echo $product ->name."有";
             if ($demand > $product -> stock){ #需求扣掉庫存得到真正需求量
                 $truedemand = $demand - $product-> stock;
+                updateProductStock($product -> name,0);
                 $product -> demand = $product -> demand + $truedemand;
                 foreach ($product -> materialList as $materialdata){
                     $index = array_search($materialdata,$product -> materialList);
-                    echo '子零件: '.$materialdata -> name.'<br/>';
-                    echo '生產所需個數: '.$product->materialnumber[$index].'<br/>';
+                    #echo '子零件: '.$materialdata -> name.'<br/>';
+                    #echo '生產所需個數: '.$product->materialnumber[$index].'<br/>';
                     array_push($tmpleadtime,countDemandandLeadtime($materialdata,$truedemand,(int)$product->materialnumber[$index]));
                 }
                 $leadtime = $leadtime + max($tmpleadtime) + $product -> leadtime;
                 $product -> maketime = max($tmpleadtime);
             }else{
                 $product -> stock = $product -> stock - $demand;
+                updateProductStock($product -> name,$product -> stock);
                 $product -> demand = 0;
             }
         //零件
@@ -127,80 +113,91 @@ class product
             #echo $product ->name."沒有";
             if ($demand > $product -> stock){ #如果需求大於庫存
                 $truedemand = $demand - $product -> stock; #需求先扣掉庫存，得到真正需要製作的需求量
+                updateProductStock($product -> name,0);
                 $product -> $demand = $product -> $demand + $truedemand;
                 $leadtime = $leadtime + $product -> leadtime; #$product ->leadtime該產品的製作時間
             }else{
                 $product -> $stock = $product -> stock - $demand;
+                updateProductStock($product -> name,$product -> stock);
                 $product -> demand = 0;
             }
         }
         return $leadtime;
     }
+    //檢查能不能延後訂貨
+    function smartstorge($product,$totaltime,$level){
+        if ($product -> materialList != null){ #isset($product -> materialList)
+            foreach($product -> materialList as $next){
+                smartstorge($next,$product -> maketime,$product -> level);
+            }
+        }else{
+            if($product -> level == $level+1){
+                $product -> maketime = $totaltime - $product -> leadtime;
+                
+            } 
+        }
+    }
+    foreach ($_POST as $value) {
+        $counter = $counter + 1;
+    }
+    $target = $_POST['target'];
+    echo '目標:'.$target;
+    echo '<br/>';
+      #print($counter.'<br>');
+    $index_counter = $counter/2;
+
+    //get weeks and demands from setDemand.php
+    for($i = 1; $i <= $index_counter ; $i = $i+1) {
+        $index_week =  'a'.(string)$i;
+        $index_demand =  'b'.(string)$i;
+        echo '周次: '.$_POST[$index_week].'需求: '.$_POST[$index_demand].'<br/>';
+    }
 
     $Alllist =[];
     array_push($Alllist,${''.$target});
-    // foreach ($Alllist as $data){
-    //     $level = 0;
-    //     echo $data -> name." : <br/>";
-    //     addlevel($data,$level);
-    // }
+    foreach ($Alllist as $data){
+        $level = 0;
+        #echo $data -> name." : <br/>";
+        addlevel($data,$level);
+    }
     
     foreach ($Alllist as $data){
         echo $data->name."'s demand : ".(string)($data->demand).'<br/>';
-        $totaltime = countDemandandLeadtime($data,1,1);
+        $totaltime = countDemandandLeadtime($data,1,100);# 第三個參數 需求總數 改變樹輸入
         echo "Total leadtime : ".(string)($totaltime).'<br/>';
-        #countdemand($data,1,1);
         echo "<br/>";
-        echo "X, demand:".$X -> demand." maketime: ".(string)($totaltime - $X -> maketime)."<br/>";
-        echo "B, demand:".$B -> demand." maketime: ".(string)($totaltime - $B -> maketime)."<br/>";
-        echo "C, demand:".$C -> demand." maketime: ".(string)($totaltime - $C -> maketime)."<br/>";
-        echo "D, demand:".$D -> demand." maketime: ".(string)($totaltime - $D -> maketime)."<br/>";
-        echo "E, demand:".$E -> demand." maketime: ".(string)($totaltime - $E -> maketime)."<br/>";
-        echo "F, demand:".$F -> demand." maketime: ".(string)($totaltime - $F -> maketime)."<br/>";
+    }
+    echo '<br/>';
+    
+    
+
+    //以物件level屬性區分出bom表成員(有計算到level);
+    $bom_list = [];
+    $material_demand = [];
+    foreach($tmpArr as $data2){
+        #echo $data.' '.gettype($data).'<br/>';
+        $index = ${''.$data2};
+        if ($index -> level != -1){
+            array_push($bom_list,$index -> name);
+            array_push($material_demand,$index -> demand);
+        }
+    }
+    foreach($bom_list as $show){
+        $index = ${''.$show};
+        // if ()
+        echo ${''.$show} -> name.'level:'.${''.$show} -> level.", demand:".${''.$show} -> demand." maketime: ".(string)($totaltime - ${''.$show} -> maketime)."<br/>";
+    }
+    foreach ($Alllist as $data2){
+        echo 'smartstorge:<br/>';
+        smartstorge($data2,0,0);
+    }
+    foreach($bom_list as $show){
+        $index = ${''.$show};
+        // if ()
+        echo ${''.$show} -> name.'level:'.${''.$show} -> level.", demand:".${''.$show} -> demand." maketime: ".(string)($totaltime - ${''.$show} -> maketime)."<br/>";
     }
 
-#echo gettype($X -> leadtime);
 
-
-
-
-// echo "B, demand:";
-// echo $B -> demand;
-// echo "<br/>";
-// echo "B, maketime:";
-// echo $B -> maketime;
-
-// echo "<br/>";
-// echo "C, demand:";
-// echo $C -> demand;
-// echo "<br/>";
-// echo "C, maketime:";
-// echo $C -> maketime;
-
-// echo "<br/>";
-// echo "D, demand:";
-// echo $D -> demand;
-// echo "<br/>";
-// echo "D, maketime:";
-// echo $D -> maketime;
-
-// echo "<br/>";
-// echo "E, demand:";
-// echo $E -> demand;
-// echo "<br/>";
-// echo "E, maketime:";
-// echo $E -> maketime;
-
-// echo "<br/>";
-// echo "F, demand:";
-// echo $F -> demand;
-// echo "<br/>";
-// echo "F, maketime:";
-// echo $F -> maketime;
-// echo "<br/>";
-// foreach ($X->materialnumber as $print){
-//     echo $print;
-// }
 #echo isset($E->materialList);
 #echo empty($X->materialList);
 #$nodeArr = array_unique($nodeArr);
